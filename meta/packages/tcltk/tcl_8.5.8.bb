openembedded/openembedded-core

commit a2e6b265ed66d5f274507dcfbb3b91c30cc12e3a

tcl: fix a build issue
This fixes:
 [BUGID #210]

This commit addes a native recipe & a patch for tcl to fix the
following build issue

| Installing message catalogs
| /bin/sh: line 2: ./tclsh: cannot execute binary file
| make: *** [install-msgs] Error 126
| FATAL: oe_runmake failed
NOTE: package tcl-8.5.8-r0: task do_install: Failed

Signed-off-by: Nitin A Kamble <nitin.a.kamble@intel.com>

meta/packages/tcltk/tcl_8.5.8.bb
@@ -2,6 +2,7 @@ DESCRIPTION = "Tool Command Language"
 LICENSE = "tcl"
 SECTION = "devel/tcltk"
 HOMEPAGE = "http://tcl.sourceforge.net"
+DEPENDS = "tcl-native"
 LIC_CHKSUM_FILES = "file://../license.terms;md5=7b4d3c71b2d9a8c1b373609867975570 \
     file://../compat/license.terms;md5=7b4d3c71b2d9a8c1b373609867975570 \
     file://../library/license.terms;md5=7b4d3c71b2d9a8c1b373609867975570 \
@@ -10,15 +11,17 @@ LIC_CHKSUM_FILES = "file://../license.terms;md5=7b4d3c71b2d9a8c1b373609867975570
     file://../win/license.terms;md5=7b4d3c71b2d9a8c1b373609867975570 \
     "
 
-PR = "r0"
+PR = "r1"
 
-SRC_URI = "\
-  ${SOURCEFORGE_MIRROR}/tcl/tcl${PV}-src.tar.gz \
-  file://tcl-add-soname.patch"
+SRC_URI = " ${SOURCEFORGE_MIRROR}/tcl/tcl${PV}-src.tar.gz \
+	  file://tcl-add-soname.patch \
+	  file://fix_non_native_build_issue.patch"
 
 S = "${WORKDIR}/tcl${PV}/unix"
 
 inherit autotools
+
+DEPENDS_virtclass-native = ""
 
 EXTRA_OECONF = "--enable-threads"
 

meta/packages/tcltk/tcl/fix_non_native_build_issue.patch
@@ -1,0 +1,63 @@
+Index: unix/Makefile.in
+===================================================================
+--- unix.orig/Makefile.in
++++ unix/Makefile.in
+@@ -622,20 +622,20 @@ ro-test: tcltest
+ shell: tclsh
+ 	@LD_LIBRARY_PATH_VAR@="`pwd`:$${@LD_LIBRARY_PATH_VAR@}"; export @LD_LIBRARY_PATH_VAR@; \
+ 	TCL_LIBRARY="${TCL_BUILDTIME_LIBRARY}"; export TCL_LIBRARY; \
+-	./tclsh $(SCRIPT)
++	tclsh $(SCRIPT)
+ 
+ # This target can be used to run tclsh inside either gdb or insight
+ gdb: tclsh
+ 	@echo "set env @LD_LIBRARY_PATH_VAR@=\"`pwd`:$${@LD_LIBRARY_PATH_VAR@}\"" > gdb.run
+ 	@echo "set env TCL_LIBRARY=${TCL_BUILDTIME_LIBRARY}" >> gdb.run
+-	$(GDB) ./tclsh --command=gdb.run
++	$(GDB) tclsh --command=gdb.run
+ 	rm gdb.run
+ 
+ # This target can be used to run tclsh inside ddd
+ ddd: tclsh
+ 	@echo "set env @LD_LIBRARY_PATH_VAR@=\"`pwd`:$${@LD_LIBRARY_PATH_VAR@}\"" > gdb.run
+ 	@echo "set env TCL_LIBRARY=${TCL_BUILDTIME_LIBRARY}" >> gdb.run
+-	$(DDD) -command=gdb.run ./tclsh
++	$(DDD) -command=gdb.run tclsh
+ 	rm gdb.run
+ 
+ VALGRINDARGS=--tool=memcheck --num-callers=8 --leak-resolution=high --leak-check=yes --show-reachable=yes -v
+@@ -648,7 +648,7 @@ valgrind: tclsh tcltest
+ valgrindshell: tclsh
+ 	@LD_LIBRARY_PATH_VAR@="`pwd`:$${@LD_LIBRARY_PATH_VAR@}"; export @LD_LIBRARY_PATH_VAR@; \
+ 	TCL_LIBRARY="${TCL_BUILDTIME_LIBRARY}"; export TCL_LIBRARY; \
+-	valgrind $(VALGRINDARGS) ./tclsh $(SCRIPT)
++	valgrind $(VALGRINDARGS) tclsh $(SCRIPT)
+ 
+ # The following target outputs the name of the top-level source directory for
+ # Tcl (it is used by Tk's configure script, for example). The .NO_PARALLEL
+@@ -817,14 +817,14 @@ install-tzdata: tclsh
+ 	@echo "Installing time zone data"
+ 	@@LD_LIBRARY_PATH_VAR@="`pwd`:$${@LD_LIBRARY_PATH_VAR@}"; export @LD_LIBRARY_PATH_VAR@; \
+ 	TCL_LIBRARY="${TCL_BUILDTIME_LIBRARY}"; export TCL_LIBRARY; \
+-	./tclsh $(TOOL_DIR)/installData.tcl \
++	tclsh $(TOOL_DIR)/installData.tcl \
+ 	    $(TOP_DIR)/library/tzdata "$(SCRIPT_INSTALL_DIR)"/tzdata
+ 
+ install-msgs: tclsh
+ 	@echo "Installing message catalogs"
+ 	@@LD_LIBRARY_PATH_VAR@="`pwd`:$${@LD_LIBRARY_PATH_VAR@}"; export @LD_LIBRARY_PATH_VAR@; \
+ 	TCL_LIBRARY="${TCL_BUILDTIME_LIBRARY}"; export TCL_LIBRARY; \
+-	./tclsh $(TOOL_DIR)/installData.tcl \
++	tclsh $(TOOL_DIR)/installData.tcl \
+ 	    $(TOP_DIR)/library/msgs "$(SCRIPT_INSTALL_DIR)"/msgs
+ 
+ install-doc: doc
+@@ -1764,7 +1764,7 @@ html-tk: tclsh
+ BUILD_HTML = \
+ 	@@LD_LIBRARY_PATH_VAR@="`pwd`:$${@LD_LIBRARY_PATH_VAR@}"; export @LD_LIBRARY_PATH_VAR@; \
+ 	TCL_LIBRARY="${TCL_BUILDTIME_LIBRARY}"; export TCL_LIBRARY; \
+-	./tclsh $(TOOL_DIR)/tcltk-man2html.tcl --htmldir="$(HTML_INSTALL_DIR)" \
++	tclsh $(TOOL_DIR)/tcltk-man2html.tcl --htmldir="$(HTML_INSTALL_DIR)" \
+ 		--srcdir=$(TOP_DIR)/.. $(BUILD_HTML_FLAGS)
+ 
+ #

ostroproject/ostro-os

commit 141cb8be1296951c32d9969aabe87924e7531e36

tcl: fix native build
commit a2e6b265ed66d5f274507dcfbb3b91c30cc12e3a introduced one patch
to fix non-native build, which however breaks native build.

(From OE-core rev: a4aaaee39de6b603e847c884d71f05cd7bbce9fe)

Signed-off-by: Kevin Tian <kevin.tian@intel.com>

meta/packages/tcltk/tcl_8.5.8.bb
@@ -11,11 +11,15 @@ LIC_CHKSUM_FILES = "file://../license.terms;md5=7b4d3c71b2d9a8c1b373609867975570
     file://../win/license.terms;md5=7b4d3c71b2d9a8c1b373609867975570 \
     "
 
-PR = "r1"
+PR = "r2"
 
-SRC_URI = " ${SOURCEFORGE_MIRROR}/tcl/tcl${PV}-src.tar.gz \
-	  file://tcl-add-soname.patch \
-	  file://fix_non_native_build_issue.patch"
+BASE_SRC_URI = "${SOURCEFORGE_MIRROR}/tcl/tcl${PV}-src.tar.gz \
+                file://tcl-add-soname.patch"
+
+SRC_URI = "${BASE_SRC_URI} \
+	   file://fix_non_native_build_issue.patch"
+
+SRC_URI_virtclass-native = "${BASE_SRC_URI}"
 
 S = "${WORKDIR}/tcl${PV}/unix"
 