openembedded/openembedded-core

commit 477ede7472db0bacd5daacb96e97f849d1be84ee

libc locale split: fix some remaining problems
* libc-{common,package}.bbclass: fix shlib renaming for the C library
	Without this you'd end up with eglibc_2.12.ipk instead of libc6_2.12.ipk as before

* eglibc-locale: don't make versions go backwards after split from eglibc
	eglibc was way beyond PR = "r1" at the time of the split, so increase PR to make package upgrades work

[RP: Fixup PR merge conflict]
Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/recipes-core/eglibc/eglibc-package.inc
@@ -90,21 +90,13 @@ inherit libc-common
 
 do_install_locale () {
 	dest=${D}/${includedir}/eglibc-locale-internal-${MULTIMACH_TARGET_SYS}
-	install -d ${dest}${base_libdir} ${dest}${bindir} ${dest}${libdir} ${dest}${datadir}
-	cp -fpPR ${D}${base_libdir}/* ${dest}${base_libdir}
-	cp -fpP ${D}${bindir}/localedef ${dest}${bindir}
-        mv ${D}${libdir}/gconv ${dest}${libdir}
-	cp -fpPR ${D}${libdir}/* ${dest}${libdir}
-        mv ${D}${datadir}/i18n ${dest}${datadir}
-	cp -fpPR ${D}${datadir}/* ${dest}${datadir}
+	install -d ${dest} ${dest}${bindir}
+	cp -fpPR ${D}${base_libdir} ${dest}${base_prefix}
+	cp -fpPR ${D}${libdir} ${dest}${exec_prefix}
+	cp -fpPR ${D}${datadir} ${dest}${exec_prefix}
+	cp -fpPR ${D}${bindir}/localedef ${dest}${bindir}
 	cp -fpPR ${WORKDIR}/SUPPORTED ${dest}
 }
 
 addtask do_install_locale after do_install before do_populate_sysroot
-
-PACKAGE_PREPROCESS_FUNCS += "eglibc_package_preprocess"
-
-eglibc_package_preprocess () {
-	rm -rf ${PKGD}/${includedir}/eglibc-locale-internal-${MULTIMACH_TARGET_SYS}
-}
 

meta/classes/libc-common.bbclass
@@ -21,3 +21,10 @@ def get_libc_fpu_setting(bb, d):
     if bb.data.getVar('TARGET_FPU', d, 1) in [ 'soft' ]:
         return "--without-fp"
     return ""
+
+python populate_packages_prepend () {
+	if bb.data.getVar('DEBIAN_NAMES', d, 1):
+		bpn = bb.data.getVar('BPN', d, 1)
+		bb.data.setVar('PKG_'+bpn, 'libc6', d)
+		bb.data.setVar('PKG_'+bpn+'-dev', 'libc6-dev', d)
+}

meta/classes/libc-package.bbclass
@@ -369,9 +369,6 @@ python package_do_split_gconvs () {
 # We want to do this indirection so that we can safely 'return'
 # from the called function even though we're prepending
 python populate_packages_prepend () {
-	if bb.data.getVar('DEBIAN_NAMES', d, 1):
-		bpn = bb.data.getVar('BPN', d, 1)
-		bb.data.setVar('PKG_'+bpn, 'libc6', d)
-		bb.data.setVar('PKG_'+bpn+'-dev', 'libc6-dev', d)
 	bb.build.exec_func('package_do_split_gconvs', d)
 }
+

meta/recipes-core/eglibc/eglibc-locale.inc
@@ -26,7 +26,7 @@ BINARY_LOCALE_ARCHES ?= "arm.* i[3-6]86 x86_64 powerpc mips"
 # set "0" for qemu emulation of native localedef for locale generation
 LOCALE_GENERATION_WITH_CROSS-LOCALEDEF = "1"
 
-PR = "r2"
+PR = "r16"
 
 PKGSUFFIX = ""
 PKGSUFFIX_virtclass-nativesdk = "-nativesdk"

ostroproject/ostro-os

commit a7627dd96c46b318bce46d855aed53df7661fc18

eglibc-package.bbclass: Fix unintended code changes
Commit 477ede7472db0bacd5daacb96e97f849d1be84ee accidentally reverted
some code changes it shouldn't have done leading to continued eglibc
packaging issues. This patch corrects that damage.

(From OE-core rev: b0dd8f5e71e33bdc2614133a7cbb21426ecee38d)

Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/recipes-core/eglibc/eglibc-package.inc
@@ -90,13 +90,21 @@ inherit libc-common
 
 do_install_locale () {
 	dest=${D}/${includedir}/eglibc-locale-internal-${MULTIMACH_TARGET_SYS}
-	install -d ${dest} ${dest}${bindir}
-	cp -fpPR ${D}${base_libdir} ${dest}${base_prefix}
-	cp -fpPR ${D}${libdir} ${dest}${exec_prefix}
-	cp -fpPR ${D}${datadir} ${dest}${exec_prefix}
-	cp -fpPR ${D}${bindir}/localedef ${dest}${bindir}
+	install -d ${dest}${base_libdir} ${dest}${bindir} ${dest}${libdir} ${dest}${datadir}
+	cp -fpPR ${D}${base_libdir}/* ${dest}${base_libdir}
+	cp -fpP ${D}${bindir}/localedef ${dest}${bindir}
+        mv ${D}${libdir}/gconv ${dest}${libdir}
+	cp -fpPR ${D}${libdir}/* ${dest}${libdir}
+        mv ${D}${datadir}/i18n ${dest}${datadir}
+	cp -fpPR ${D}${datadir}/* ${dest}${datadir}
 	cp -fpPR ${WORKDIR}/SUPPORTED ${dest}
 }
 
 addtask do_install_locale after do_install before do_populate_sysroot
+
+PACKAGE_PREPROCESS_FUNCS += "eglibc_package_preprocess"
+
+eglibc_package_preprocess () {
+	rm -rf ${PKGD}/${includedir}/eglibc-locale-internal-${MULTIMACH_TARGET_SYS}
+}
 

meta/recipes-core/eglibc/eglibc_2.13.bb
@@ -4,7 +4,7 @@ SRCREV = "14157"
 
 DEPENDS += "gperf-native"
 FILESPATHPKG =. "eglibc-svn:"
-PR = "r4"
+PR = "r5"
 PR_append = "+svnr${SRCPV}"
 
 EGLIBC_BRANCH="eglibc-2_13"