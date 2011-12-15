openembedded/openembedded-core

commit 2078af333d704fd894a2dedbc19cef5775cdadbb

image.bbclass/initscripts: Make /etc/timestamp usage consistent.
 Timestamp is kept in UTC
 Remove superfluous 'create_etc_timestamp()' function
 - seems to be a duplicate of 'rootfs_update_timestamp()'
 Remove External function reference

[sgw@linux.intel.com: merged 2 patches and cleanup commit message]

Signed-off-by: Gary Thomas <gary@mlbassoc.com>
Signed-off-by: Saul Wold <sgw@linux.intel.com>

meta/recipes-core/initscripts/initscripts-1.0/bootmisc.sh
@@ -65,13 +65,13 @@ fi
 # If the timestamp is 1 day or more recent than the current time,
 # use the timestamp instead.
 /etc/init.d/hwclock.sh start
-if test -e /etc/timestamp2
+if test -e /etc/timestamp
 then
-	SYSTEMDATE=`date "+%Y%m%d%2H%2M"`
-	read TIMESTAMP < /etc/timestamp2
+	SYSTEMDATE=`date  -u +%2m%2d%2H%2M%4Y`
+	read TIMESTAMP < /etc/timestamp
         NEEDUPDATE=`expr \( $TIMESTAMP \> $SYSTEMDATE + 10000 \)`
         if [ $NEEDUPDATE -eq 1 ]; then 
-		date -s $TIMESTAMP
+		date -u $TIMESTAMP
 		/etc/init.d/hwclock.sh stop
 	fi
 fi

meta/classes/image.bbclass
@@ -182,10 +182,6 @@ zap_root_password () {
 	mv ${IMAGE_ROOTFS}/etc/passwd.new ${IMAGE_ROOTFS}/etc/passwd
 } 
 
-create_etc_timestamp() {
-	date +%2m%2d%2H%2M%Y >${IMAGE_ROOTFS}/etc/timestamp
-}
-
 # Turn any symbolic /sbin/init link into a file
 remove_init_link () {
 	if [ -h ${IMAGE_ROOTFS}/sbin/init ]; then
@@ -217,7 +213,7 @@ set_image_autologin () {
 # Can be use to create /etc/timestamp during image construction to give a reasonably 
 # sane default time setting
 rootfs_update_timestamp () {
-	date "+%m%d%H%M%Y" >${IMAGE_ROOTFS}/etc/timestamp
+	date -u +%2m%2d%2H%2M%4Y >${IMAGE_ROOTFS}/etc/timestamp
 }
 
 # Prevent X from being started
@@ -239,7 +235,7 @@ rootfs_trim_schemas () {
 }
 
 
-# export the zap_root_password, create_etc_timestamp and remote_init_link
-EXPORT_FUNCTIONS zap_root_password create_etc_timestamp remove_init_link do_rootfs make_zimage_symlink_relative set_image_autologin rootfs_update_timestamp rootfs_no_x_startup
+# export the zap_root_password, and remote_init_link
+EXPORT_FUNCTIONS zap_root_password remove_init_link do_rootfs make_zimage_symlink_relative set_image_autologin rootfs_update_timestamp rootfs_no_x_startup
 
 addtask rootfs before do_build after do_install

meta/recipes-core/initscripts/initscripts-1.0/save-rtc.sh
@@ -10,4 +10,4 @@
 ### END INIT INFO
 
 # Update the timestamp
-date +%Y%m%d%2H%2M > /etc/timestamp2
+date -u +%2m%2d%2H%2M%4Y > /etc/timestamp

ostroproject/ostro-os

commit b780cccb858f912e72b19fca52f0c4df4d107c59

initscripts: fix timestamp checking at bootmisc.sh
Timestamp checking has been broken by the commit
2078af333d704fd894a2dedbc19cef5775cdadbb. Currently the RTC time
is always overwritten with the time from /etc/timestmap. Fix timestamp
checking and clean the code.

(From OE-core rev: cc8695f22bc70ef958f81d0d3da73dece5f4700a)

Signed-off-by: Lauri Hintsala <lauri.hintsala@bluegiga.com>

meta/recipes-core/initscripts/initscripts-1.0/bootmisc.sh
@@ -62,15 +62,14 @@ then
 fi
 
 # Set the system clock from hardware clock
-# If the timestamp is 1 day or more recent than the current time,
+# If the timestamp is more recent than the current time,
 # use the timestamp instead.
 /etc/init.d/hwclock.sh start
 if test -e /etc/timestamp
 then
-	SYSTEMDATE=`date  -u +%2m%2d%2H%2M%4Y`
+	SYSTEMDATE=`date -u +%4Y%2m%2d`
 	read TIMESTAMP < /etc/timestamp
-        NEEDUPDATE=`expr \( $TIMESTAMP \> $SYSTEMDATE + 10000 \)`
-        if [ $NEEDUPDATE -eq 1 ]; then 
+	if [ ${TIMESTAMP#????????}${TIMESTAMP%????????} -gt $SYSTEMDATE ]; then
 		date -u $TIMESTAMP
 		/etc/init.d/hwclock.sh stop
 	fi