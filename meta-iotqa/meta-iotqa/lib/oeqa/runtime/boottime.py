2beac3cebfbf5c12ea2257d41452201328f88503
Li Min <min.a.li@intel.com>
2015-05-11 15:09:34 +0800

    meta-iotqa: Add systemd-analyze file and log file for boot time
    From meta-iotqa rev: 49b94e8559ea724576e3c868423a9cd0f42946e9

--- meta-iotqa/meta-iotqa/lib/oeqa/runtime/boottime.py
+++ meta-iotqa/meta-iotqa/lib/oeqa/runtime/boottime.py
@@ -1,18 +1,24 @@
 #[PROTEXCAT]
 #\License: ALL RIGHTS RESERVED
 
-'''System boot time'''
+"""System boot time"""
+import os
 from oeqa.oetest import oeRuntimeTest
+from oeqa.runtime.helper import collect_pnp_log
 
 class BootTimeTest(oeRuntimeTest):
-    
+
     def _setup(self):
-        (status,output) = self.target.copy_to("systemd-analyze","/tmp/systemd-analyze")
+        (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,'systemd-analyze'),"/tmp/systemd-analyze")
         self.assertEqual(status, 0, msg="systemd-analyze could not be copied. Output: %s" % output)
         (status,output) = self.target.run(" ls -la /tmp/systemd-analyze")
         self.assertEqual(status, 0, msg="Failed to find systemd-analyze command")
 
     def test_boot_time(self):
         self._setup()
+        filename=os.path.basename(__file__)
+        casename=os.path.splitext(filename)[0]
         (status,output) = self.target.run("/tmp/systemd-analyze time | awk -F '=' '{print $2}'")
+        collect_pnp_log(casename, output)        
+        print "\n%s:%s\n" %(casename, output)
         self.assertEqual(status, 0, output)
