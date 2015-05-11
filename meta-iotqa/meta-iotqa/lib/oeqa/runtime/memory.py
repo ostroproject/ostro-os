6e1248696542a177bcd0da6d5e130469ce283f5a
Li Min <min.a.li@intel.com>
2015-05-11 15:06:52 +0800

    meta-iotqa: Update memory test method,add memory log file
    From meta-iotqa rev: fff9cf6b8a4aeaee9a7dc91cd9cfb9c4ce2d8a18

--- meta-iotqa/meta-iotqa/lib/oeqa/runtime/memory.py
+++ meta-iotqa/meta-iotqa/lib/oeqa/runtime/memory.py
@@ -1,24 +1,32 @@
 #[PROTEXCAT]
 #\License: ALL RIGHTS RESERVED
 
-'''Memory consumption for system idle'''
+"""Memory consumption for system idle"""
+import os
 import time
 from oeqa.oetest import oeRuntimeTest
+from oeqa.runtime.helper import collect_pnp_log
 
 class MemTest(oeRuntimeTest):
     
     def _reboot(self):
-        ''' reboot device for clean env '''
+        """reboot device for clean env"""
         (status, output) = self.target.run("reboot")
-        time.sleep(60)
+        time.sleep(120)
         self.assertEqual(status, 0, output)
 
     def _setup(self):
-        (status,output) = self.target.run("sleep 300 && sync && echo 1 >/proc/sys/vm/drop_cache")
+        """First set sleep time to 180s (default 300s)"""
+        (status,output) = self.target.run("sleep 180 && sync && echo 1 >/proc/sys/vm/drop_caches")
         self.assertEqual(status, 0, output)
 
     def test_mem(self):
-        self._reboot()
+        #self._reboot()
         self._setup()
+        filename=os.path.basename(__file__)
+        casename=os.path.splitext(filename)[0]
         (status,output) = self.target.run("free | grep 'Mem' | awk '{print $3}'")
+        collect_pnp_log(casename, output)        
+        print "\n%s:%s\n" %(casename, output)
         self.assertEqual(status, 0, output)
+  
