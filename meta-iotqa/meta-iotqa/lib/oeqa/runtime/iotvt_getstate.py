4fb978b2266ff2c2ed47a74bdbca6ded9eed45da
Zhang Jingke <jingke.zhang@intel.com>
2015-05-12 21:44:05 +0800

    meta-iotqa: Add initialization steps for all 5 iotivity cases.
    (From meta-iotqa rev: d2563717cdc9eb28c90ca994aede772f9d41e674)
    
    Signed-off-by: Zhang Jingke <jingke.zhang@intel.com>

--- meta-iotqa/meta-iotqa/lib/oeqa/runtime/iotvt_getstate.py
+++ meta-iotqa/meta-iotqa/lib/oeqa/runtime/iotvt_getstate.py
@@ -1,15 +1,20 @@
+import os
 from oeqa.oetest import oeRuntimeTest
 
 class IOtvtClient(oeRuntimeTest):
     '''Iotivity client get resource state'''
     def test_iotvt_getstate(self):
         '''Prepare test binaries to image'''
-        (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
+        (status, output) = self.target.run('mkdir -p /opt/iotivity-test/apps/iotivity-test/')
+        (status, output) = self.target.run('ls /opt/iotivity-test/apps/iotivity-test/servertest')
+        if status != 0:
+            (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
                           'servertest'), "/opt/iotivity-test/apps/iotivity-test/")
-        (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
+            (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
                           'clienttest'), "/opt/iotivity-test/apps/iotivity-test/")
 
         '''start iotivity server to register a new resource'''
+        (status, output) = self.target.run("ps | grep servertest | awk '{print $1}' | xargs kill -9")
         reg_cmd = "/opt/iotivity-test/apps/iotivity-test/servertest > /dev/null 2>&1 &"
         (status, output) = self.target.run(reg_cmd)
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
