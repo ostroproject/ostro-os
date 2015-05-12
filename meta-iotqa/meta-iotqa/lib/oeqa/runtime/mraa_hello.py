cb96e0989f89eac15bc0a714e8893d4dd05d66ca
Zhang Jingke <jingke.zhang@intel.com>
2015-05-13 01:04:57 +0800

    meta-iotqa: update mraa_test to mraa_hello
    From meta-iotqa rev: 99c56ae55e243ef1c3513e96252e5c198fd3d811

--- meta-iotqa/meta-iotqa/lib/oeqa/runtime/mraa_hello.py
+++ meta-iotqa/meta-iotqa/lib/oeqa/runtime/mraa_hello.py
@@ -6,12 +6,11 @@ class Mraa_hello(oeRuntimeTest):
     def test_mraa_hello(self):
         '''Prepare test binaries to image'''
         (status, output) = self.target.run('mkdir -p /opt/mraa-test/apps/')
-        (status, output) = self.target.run('ls /opt/mraa-test/apps/mraa-test')
+        (status, output) = self.target.run('ls /opt/mraa-test/apps/hello_mraa')
         if status != 0:
-            (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
-                          'mraa-test'), "/opt/mraa-test/apps/")
+            (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,'hello_mraa'), "/opt/mraa-test/apps/")
         '''run test mraa app to get the platform information'''
-        client_cmd = "/opt/mraa-test/apps/mraa-test"
+        client_cmd = "/opt/mraa-test/apps/hello_mraa"
         (status, output) = self.target.run(client_cmd)
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
 
