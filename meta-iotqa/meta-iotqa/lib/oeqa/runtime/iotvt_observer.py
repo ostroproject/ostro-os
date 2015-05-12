7477d2795c591a305aab2b40f92dca189af664ff
Zhang Jingke <jingke.zhang@intel.com>
2015-05-12 17:08:07 +0800

    meta-iotqa: Add clienttest and servertest for IoTivity. Do file copy in every iotivity case, to deploy the 2 binaries. Modify comm_ssh.py.
    (From meta-iotqa rev: 22ff73e57bdcd053bc386d2e3414231994098118)
    
    Signed-off-by: Zhang Jingke <jingke.zhang@intel.com>

--- meta-iotqa/meta-iotqa/lib/oeqa/runtime/iotvt_observer.py
+++ meta-iotqa/meta-iotqa/lib/oeqa/runtime/iotvt_observer.py
@@ -3,8 +3,14 @@ from oeqa.oetest import oeRuntimeTest
 class IOtvtClient(oeRuntimeTest):
     '''Iotivity client set observer'''
     def test_iotvt_observer(self):
+        '''Prepare test binaries to image'''
+        (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
+                          'servertest'), "/opt/iotivity-test/apps/iotivity-test/")
+        (status,output) = self.target.copy_to(os.path.join(oeRuntimeTest.tc.filesdir,
+                          'clienttest'), "/opt/iotivity-test/apps/iotivity-test/")
+
         '''start iotivity server to register a new resource'''
-        reg_cmd = "/opt/iotivity-test/apps/iotivity-test/servertesti > /dev/null 2>&1 &"
+        reg_cmd = "/opt/iotivity-test/apps/iotivity-test/servertest > /dev/null 2>&1 &"
         (status, output) = self.target.run(reg_cmd)
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
         
