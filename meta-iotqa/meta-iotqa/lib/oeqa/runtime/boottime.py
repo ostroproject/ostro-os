121be0b96883f1d451a045c8a52d8cbeb64888e9
Li Min <min.a.li@intel.com>
2015-05-27 15:25:58 +0800

    meta-iotqa: Clean the code with autopen8
    From meta-iotqa rev: 65266813c6438eeb67002e17dd4cd70a00e84b5d

--- meta-iotqa/meta-iotqa/lib/oeqa/runtime/boottime.py
+++ meta-iotqa/meta-iotqa/lib/oeqa/runtime/boottime.py
@@ -6,19 +6,30 @@ import os
 from oeqa.oetest import oeRuntimeTest
 from oeqa.runtime.helper import collect_pnp_log
 
+
 class BootTimeTest(oeRuntimeTest):
 
     def _setup(self):
-        (status,output) = self.target.copy_to(os.path.join(os.path.dirname(__file__), 'files','systemd-analyze'),"/tmp/systemd-analyze")
-        self.assertEqual(status, 0, msg="systemd-analyze could not be copied. Output: %s" % output)
-        (status,output) = self.target.run(" ls -la /tmp/systemd-analyze")
-        self.assertEqual(status, 0, msg="Failed to find systemd-analyze command")
+        (status, output) = self.target.copy_to(
+            os.path.join(os.path.dirname(__file__), 'files',
+                         'systemd-analyze'), "/tmp/systemd-analyze")
+        self.assertEqual(
+            status,
+            0,
+            msg="systemd-analyze could not be copied. Output: %s" %
+            output)
+        (status, output) = self.target.run(" ls -la /tmp/systemd-analyze")
+        self.assertEqual(
+            status,
+            0,
+            msg="Failed to find systemd-analyze command")
 
     def test_boot_time(self):
         self._setup()
-        filename=os.path.basename(__file__)
-        casename=os.path.splitext(filename)[0]
-        (status,output) = self.target.run("/tmp/systemd-analyze time | awk -F '=' '{print $2}'")
-        collect_pnp_log(casename, output)        
-        print "\n%s:%s\n" %(casename, output)
+        filename = os.path.basename(__file__)
+        casename = os.path.splitext(filename)[0]
+        (status, output) = self.target.run("/tmp/systemd-analyze time"
+                                           " | awk -F '=' '{print $2}'")
+        collect_pnp_log(casename, output)
+        print "\n%s:%s\n" % (casename, output)
         self.assertEqual(status, 0, output)
