27684c9931e14229b16aa8c41e56759bbf3b1ed4
root <yan.ma@intel.com>
2015-07-10 11:49:15 -0400

    meta-iotqa: sensor framework test: integrate feature verification test cases for IOTOS-504
    TCs: test_get_all_installed_sensor,test_get_sensor_list_by_invalid_type,test_get_sensor_list_by_type
    Corresponding c file:test_get_all_installed_sensor, test_get_sensor_list_by_type
    Related to: IOTOS-504
    (From meta-iotqa rev: 346ce7a8864b1c8df0f0dc58e7e19b4eea305e17)
    
    Signed-off-by: root <yan.ma@intel.com>

---
+++ meta-iotqa/lib/oeqa/runtime/sensor_FW/test_get_all_installed_sensor.py
@@ -0,0 +1,18 @@
+'''Verify to get all sensors in system trhough api sf_get_sensor_list()'''
+import os
+from oeqa.oetest import oeRuntimeTest
+
+class TestGetAllInstalledSensor(oeRuntimeTest):
+    '''Verify all sensors installed in system'''
+    def test(self):
+        '''push binary to target and run with argument'''
+        mkdir_path = "mkdir -p /opt/sensor-test/apps"
+        (status, output) = self.target.run(mkdir_path)
+        copy_to_path = os.path.join(get_files_dir(), 'test_get_all_installed_sensor')
+        (status, output) = self.target.copy_to(copy_to_path, \
+"/opt/sensor-test/apps/")
+        #run test get all installed sensor and show it's information
+        client_cmd = "/opt/sensor-test/apps/test_get_all_installed_sensor"
+        (status, output) = self.target.run(client_cmd)
+        print output
+        self.assertEqual(status, 1, msg="Error messages: %s" % output)
