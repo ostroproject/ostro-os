aec6cec5740347f969462eeab464fd1a38615137
root <yan.ma@intel.com>
2015-07-10 11:55:42 -0400

    meta-iotqa: sensor framework test: integrate feature verification test cases for IOTOS-520
    TCs:test_get_default_sensor_by_type, test_get_default_sensor_by_invalid_type
    Corresponding c file: test_get_default_sensor_by_type.c
    
    Related to: IOTOS-520
    
    (From meta-iotqa rev: 5dc635f6b7a1a05d634abffa93e26730009c8f68)
    
    Signed-off-by: root <yan.ma@intel.com>

---
+++ meta-iotqa/lib/oeqa/runtime/sensor_FW/test_get_default_sensor_by_type.py
@@ -0,0 +1,20 @@
+'''Verify to get default sensor with specific type trhough api sf_get_default_sensor()'''
+import os
+from oeqa.oetest import oeRuntimeTest
+import readConfigFile
+
+class TestGetDefaultSensorByType(oeRuntimeTest):
+    '''Verify default sensor returned from sf_get_default_sensor()'''
+    def test(self):
+        '''push binary to target and run with argument'''
+        mkdir_path = "mkdir -p /opt/sensor-test/apps"
+        (status, output) = self.target.run(mkdir_path)
+        copy_to_path = os.path.join(get_files_dir(), 'test_get_default_sensor_by_type')
+        (status, output) = self.target.copy_to(copy_to_path, \
+"/opt/sensor-test/apps/")
+        #run test get expected sensor and show it's information
+        client_cmd = "/opt/sensor-test/apps/test_get_default_sensor_by_type "\
+                     + readConfigFile.ReadConfFile.getSectionValue( 'sensors','sensor-type-id')
+        (status, output) = self.target.run(client_cmd)
+        print output
+        self.assertEqual(status, 1, msg="Error messages: %s" % output)
