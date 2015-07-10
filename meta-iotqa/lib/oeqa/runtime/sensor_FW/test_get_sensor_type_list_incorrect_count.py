10977aae67076fe9216363823086484202aaa527
root <yan.ma@intel.com>
2015-07-10 12:07:58 -0400

    meta-iotqa: sensor framework test: integrate feature verification test cases for IOTOS-512
    TCs: test_get_sensor_type_list, test_get_sensor_type_list_incorrect_count
    Corresponding c file: test_get_sensor_type_list.c, test_get_sensor_type_list_incorrect_count.c
    
    Related to: IOTOS-512
    
    (From meta-iotqa rev: cf630f0429396e4dfb5ff1650a6a1f4e25b6a8c8)
    
    Signed-off-by: root <yan.ma@intel.com>

---
+++ meta-iotqa/lib/oeqa/runtime/sensor_FW/test_get_sensor_type_list_incorrect_count.py
@@ -0,0 +1,20 @@
+'''Verify error handling when input parameter 'count' is not correct'''
+import os
+import readConfigFile
+from oeqa.oetest import oeRuntimeTest
+
+class TestGetSensorTypeListIncorrectCount(oeRuntimeTest):
+    '''Verify error code return if input paramter 'count' is not correct'''
+    def test(self):
+        '''push binary to target and run it with argument'''
+        #Prepare test binaries to image        
+        mkdir_path = "mkdir -p /opt/sensor-test/apps/"
+        (status, output) = self.target.run(mkdir_path)
+        copy_path = os.path.join(get_files_dir(), \
+ 'test_get_sensor_type_list_incorrect_count')
+        (status, output) = self.target.copy_to(copy_path, \
+"/opt/sensor-test/apps/")
+        client_cmd = "/opt/sensor-test/apps/test_get_sensor_type_list_incorrect_count "
+        (status, output) = self.target.run(client_cmd)
+        print output
+        self.assertEqual(status, 0, msg="Error messages: %s" % output)
