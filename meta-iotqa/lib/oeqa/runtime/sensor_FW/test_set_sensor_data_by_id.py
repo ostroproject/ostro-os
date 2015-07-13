b0407241335b7c557322cd8c3ff9d1527603cbf0
“yma1” <yan.ma@intel.com>
2015-07-13 13:20:40 -0400

    meta-iotqa: sensor framework test: integrate feature verification test cases for IOTOS-517
    TCs: test_set_sensor_data_by_id, test_set_sensor_data_by_invalid_id
    Corresponding c files: test_set_sensor_data_by_id.c
    
    Related to: IOTOS-517
    
    (From meta-iotqa rev: 25475b96c408bb76e47c9c83449d87e9889db0d0)
    
    Signed-off-by: “yma1” <yan.ma@intel.com>

---
+++ meta-iotqa/lib/oeqa/runtime/sensor_FW/test_set_sensor_data_by_id.py
@@ -0,0 +1,20 @@
+'''Verify get sensor data based on its id'''
+import os
+from oeqa.oetest import oeRuntimeTest
+import readConfigFile
+
+class TestGetSensorDataById(oeRuntimeTest):
+    '''Verify sensor data can be returned based on sensor id'''
+    def test(self):
+        '''push binary to target and run it with argument'''
+        #Prepare test binaries to image
+        mkdir_path = "mkdir -p /opt/sensor-test/apps/"
+        (status, output) = self.target.run(mkdir_path)
+        copy_to_path = os.path.join(get_files_dir(), 'test_set_sensor_data_by_id')
+        (status, output) = self.target.copy_to(copy_to_path, "/opt/sensor-test/apps/")
+        #run test set sensor data by id and show it's information
+        client_cmd = "/opt/sensor-test/apps/test_set_sensor_data_by_id " \
+                     + readConfigFile.ReadConfFile.getSectionValue( 'sensors','valid-id')
+        (status, output) = self.target.run(client_cmd)
+        print output
+        self.assertEqual(status, 1, msg="Error messages: %s" % output)
---
+++ meta-iotqa/lib/oeqa/runtime/sensor_FW/test_set_sensor_data_by_invalid_id.py
@@ -0,0 +1,20 @@
+'''Verify fail to set sensor data if given id is invalid'''
+import os
+from oeqa.oetest import oeRuntimeTest
+import readConfigFile
+
+class TestGetSensorDataByInvalidId(oeRuntimeTest):
+    '''Verify error happens when set data using invalid sensor id'''
+    def test(self):
+        '''push binary to target and run it with argument'''
+        #Prepare test binaries to image
+        mkdir_path = "mkdir -p /opt/sensor-test/apps/"
+        (status, output) = self.target.run(mkdir_path)
+        copy_to_path = os.path.join(get_files_dir(), 'test_set_sensor_data_by_id')
+        (status, output) = self.target.copy_to(copy_to_path, "/opt/sensor-test/apps/")
+        #run test set sensor data by invalid id and show it's information
+        client_cmd = "/opt/sensor-test/apps/test_set_sensor_data_by_id " \
+                     + readConfigFile.ReadConfFile.getSectionValue( 'sensors','invalid-id')
+        (status, output) = self.target.run(client_cmd)
+        print output
+        self.assertEqual(status, 0, msg="Error messages: %s" % output)
