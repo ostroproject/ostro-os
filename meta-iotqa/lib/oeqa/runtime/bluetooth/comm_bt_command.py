5026c4f4e44f76a638d9a2817d5708c3430c8e0d
Zhang Jingke <jingke.zhang@intel.com>
2015-08-14 21:01:18 +0800

    meta-iotqa: combine comm_bt_hcitest.py to comm_bt_command.py
    Remove old comm_bt_hcitest.py, combine all the five cases to the
    new comm_bt_command.py. The total number is not changed.
    In new comm_bt_command.py, cases are mostly implemented by expect.
    
    (From meta-iotqa rev: fe2d54b8fdd06eca0f4a5a970f745f5c6809322d)
    
    Signed-off-by: Zhang Jingke <jingke.zhang@intel.com>

--- meta-iotqa/lib/oeqa/runtime/bluetooth/comm_bt_command.py
+++ meta-iotqa/lib/oeqa/runtime/bluetooth/comm_bt_command.py
@@ -1,4 +1,5 @@
 import os
+import time
 import subprocess
 from oeqa.oetest import oeRuntimeTest
 from oeqa.utils.helper import shell_cmd_timeout
@@ -12,8 +13,12 @@ class CommBTTest(oeRuntimeTest):
         self.target.run('rfkill unblock all')
         self.target.run('hciconfig hci0 up')
         self.target.run('hciconfig hci0 piscan')
-        status, output = shell_cmd_timeout('hciconfig hci0 up', timeout=200)
-        status, output = shell_cmd_timeout('hciconfig hci0 piscan', timeout=200)
+        self.target.run('hciconfig hci0 noleadv')
+        time.sleep(1)
+        status, output = shell_cmd_timeout('hciconfig hci0 up', timeout=100)
+        status, output = shell_cmd_timeout('hciconfig hci0 piscan', timeout=100)
+        status, output = shell_cmd_timeout('hciconfig hci0 noleadv', timeout=100)
+        time.sleep(1)
 
     @tag(FeatureID="IOTOS-453")
     def test_bt_pairing(self):
@@ -27,7 +32,9 @@ class CommBTTest(oeRuntimeTest):
         master_exp = os.path.join(os.path.dirname(__file__), "files/bt_pair_master.exp")
         (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
         cmd = "expect %s %s" % (master_exp, target_btmac)
+        print cmd
         status, output = shell_cmd_timeout(cmd, timeout=200)
+        self.assertEqual(status, 2, msg="expect excution fail")
 
         # On Host, check paired devices to see if IoT is in
         check_exp = os.path.join(os.path.dirname(__file__), "files/bt_list_paired_device.exp")
@@ -39,6 +46,7 @@ class CommBTTest(oeRuntimeTest):
         '''Use gatttool to show target primary attr handles'''
         # On target, do LE advertising
         self.target.run('hciconfig hci0 leadv')
+        time.sleep(1)
         # Host does gatttool commands
         (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
         cmd = "gatttool -b %s --primary | grep '^attr handle'" % target_btmac
@@ -50,6 +58,7 @@ class CommBTTest(oeRuntimeTest):
         '''Use gatttool to show target characteristics handles'''
         # On target, do LE advertising
         self.target.run('hciconfig hci0 leadv')
+        time.sleep(1)
         # Host does gatttool commands
         (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
         cmd = "gatttool -b %s --characteristics | grep '^handle'" % target_btmac
@@ -61,6 +70,7 @@ class CommBTTest(oeRuntimeTest):
         '''Use gatttool to read target handle value'''
         # On target, do LE advertising
         self.target.run('hciconfig hci0 leadv')
+        time.sleep(1)
         # Host does gatttool commands
         (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
         cmd = "gatttool -b %s --char-read -a 0x0002 | grep '02 03 00 00 2a'" % target_btmac
@@ -72,14 +82,88 @@ class CommBTTest(oeRuntimeTest):
         '''Use gatttool interactive mode to do connect'''
         # On target, do LE advertising
         self.target.run('hciconfig hci0 leadv')
+        time.sleep(1)
         # Host does gatttool commands
         (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
         connect_exp = os.path.join(os.path.dirname(__file__), "files/gatt_connect.exp")
         cmd = "expect %s %s" % (connect_exp, target_btmac)
         status, output = shell_cmd_timeout(cmd, timeout=200)
-        if ("CON" in output):
-            print "connect succeeds"
-            return 0
-        else:
-            print "connect fails"
-            return 1
+        self.assertEqual(status, 2, msg="gatttool connect target fails: %s" % output) 
+
+    @tag(FeatureID="IOTOS-453")
+    def test_bt_power_on(self):
+        '''enable bluetooth device'''
+        self.target.run('hciconfig hci0 down')
+        # start bluetoothctl, then input 'power on'
+        exp = os.path.join(os.path.dirname(__file__), "files/power_on.exp")
+        target_ip = self.target.ip
+        status, output = shell_cmd_timeout('expect %s %s' % (exp, target_ip), timeout=200)
+        self.assertEqual(status, 2, msg="power on command fails: %s" % output)
+        # check it again with hciconfig
+        (status, output) = self.target.run("hciconfig hci0 | grep 'UP RUNNING'")
+        self.assertEqual(status, 0, msg="%s" % output)
+
+    @tag(FeatureID="IOTOS-453")
+    def test_bt_visable_on(self):
+        '''enable visibility'''
+        self.target.run('hciconfig hci0 noscan')
+        # start bluetoothctl, then input 'discoverable on'
+        exp = os.path.join(os.path.dirname(__file__), "files/discoverable_on.exp")
+        target_ip = self.target.ip
+        status, output = shell_cmd_timeout('expect %s %s' % (exp, target_ip), timeout=200)
+        self.assertEqual(status, 2, msg="discoverable on command fails: %s" % output)
+        # check it again with hciconfig
+        (status, output) = self.target.run("hciconfig hci0 | grep 'ISCAN'")
+        self.assertEqual(status, 0, msg="%s" % output)
+
+    @tag(FeatureID="IOTOS-456")
+    def test_bt_visible_scan(self):
+        '''Scan nearby bluetooth devices (not ble scan)'''
+        # Close target's leadv
+        shell_cmd_timeout('hciconfig hci0 reset', timeout=100)
+        self.target.run('hciconfig hci0 reset')
+        self.target.run('hciconfig hci0 noleadv')
+        self.target.run('hciconfig hci0 piscan')
+        time.sleep(1)
+        (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
+        # start bluetoothctl to scan target 
+        exp = os.path.join(os.path.dirname(__file__), "files/bt_scan.exp")
+        cmd = "expect %s %s" % (exp, target_btmac)
+        status, output = shell_cmd_timeout(cmd, timeout=100)
+        shell_cmd_timeout('hciconfig hci0 reset', timeout=100)
+        self.target.run('hciconfig hci0 reset')
+        self.assertEqual(status, 2, msg="scan target fails: %s" % output) 
+
+    @tag(FeatureID="IOTOS-759")
+    def test_bt_leadv(self):
+        '''Target does LE advertising, Host scan target'''
+        # close target piscan firstly, and then enable leadv
+        shell_cmd_timeout('hciconfig hci0 reset', timeout=100)
+        self.target.run('hciconfig hci0 reset')
+        self.target.run('hciconfig hci0 leadv')
+        time.sleep(1)
+        (status, target_btmac) = self.target.run("hciconfig | grep 'BD Address' | awk '{print $3}'")
+        # start bluetoothctl to scan target 
+        exp = os.path.join(os.path.dirname(__file__), "files/bt_lescan.exp")
+        cmd = "expect %s %s" % (exp, target_btmac)
+        status, output = shell_cmd_timeout(cmd, timeout=100)
+        shell_cmd_timeout('hciconfig hci0 reset', timeout=100)
+        self.target.run('hciconfig hci0 reset')
+        self.assertEqual(status, 2, msg="scan target leadv fails: %s" % output) 
+
+    @tag(FeatureID="IOTOS-770")
+    def test_bt_le_scan(self):
+        '''Another device (host) does LE advertising, target scan'''
+        # close host piscan firstly, and then enable leadv
+        shell_cmd_timeout('hciconfig hci0 reset', timeout=100)
+        self.target.run('hciconfig hci0 reset')
+        status, output = shell_cmd_timeout('hciconfig hci0 leadv 3', timeout=100)
+        time.sleep(1)
+        (status, host_btmac) = shell_cmd_timeout("hciconfig | grep 'BD Address' | awk '{print $3}'", timeout=100)
+        # From target, start bluetoothctl to scan host
+        exp = os.path.join(os.path.dirname(__file__), "files/bt_target_lescan.exp")
+        cmd = "expect %s %s %s" % (exp, self.target.ip, host_btmac)
+        status, output = shell_cmd_timeout(cmd, timeout=100)
+        shell_cmd_timeout('hciconfig hci0 reset', timeout=100)
+        self.target.run('hciconfig hci0 reset')
+        self.assertEqual(status, 2, msg="scan host leadv fails: %s" % output) 
