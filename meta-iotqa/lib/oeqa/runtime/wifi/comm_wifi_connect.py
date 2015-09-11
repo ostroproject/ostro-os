cdcf601b5b98a34b1285625639224188547ca09c
Zhang Jingke <jingke.zhang@intel.com>
2015-09-11 18:38:02 +0800

    meta-iotqa: add two WiFi cases to connect to broadcast AP
    Add case to connect to a broadcasting AP.
    Add case to ping internet web-link.
    
    (From meta-iotqa rev: 1430d5096394046994da3b2930e2c20479b4d79a)
    
    Signed-off-by: Zhang Jingke <jingke.zhang@intel.com>

--- meta-iotqa/lib/oeqa/runtime/wifi/comm_wifi_connect.py
+++ meta-iotqa/lib/oeqa/runtime/wifi/comm_wifi_connect.py
@@ -23,15 +23,16 @@ class CommWiFiConect(oeRuntimeTest):
         time.sleep(20)
         # Scan nearby to get service of none-encryption broadcasting ssid
         hidden_str = "hidden_managed_psk"
-        (status, output) = self.target.run('connmanctl scan wifi')
-        self.assertEqual(status, 0, msg="Error messages: %s" % output)
-        (status, services) = self.target.run("connmanctl services | grep %s" % hidden_str)
-        # will do scan retry 1 time if needed
-        if (status != 0):
+        # will do scan retry 3 times if needed
+        retry = 0
+        while (retry < 4):
             (status, output) = self.target.run('connmanctl scan wifi')
             self.assertEqual(status, 0, msg="Error messages: %s" % output)
             (status, services) = self.target.run("connmanctl services | grep %s" % hidden_str)
-            self.assertEqual(status, 0, msg="Not found hidden AP service")
+            retry = retry + 1
+            if (status == 0):
+                break
+        self.assertEqual(status, 0, msg="Not found hidden AP service")
         self.hidden_service = services.strip()
 
     def tearDown(self):
@@ -122,3 +123,65 @@ class CommWiFiConect(oeRuntimeTest):
         (status, wifi_interface) = self.target.run("ifconfig | grep '^wlp' | awk '{print $1}'")
         (status, output) = self.target.run("ifconfig %s | grep 'inet addr:'" % wifi_interface)
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
+
+    @tag(FeatureID="IOTOS-490")
+    def test_wifi_connect_wpa2psk_broadcast(self):
+        '''connmanctl to connect WPA2-PSK wifi AP (not hidden)'''
+        target_ip = self.target.ip 
+        ssid = ssid_config.get("Connect","ssid_broadcast")
+        pwd = ssid_config.get("Connect","passwd_broadcast")
+
+        # For broadcast AP, get its service firstly.
+        retry = 0
+        while (retry < 4):
+            (status, output) = self.target.run('connmanctl scan wifi')
+            self.assertEqual(status, 0, msg="Error messages: %s" % output)
+            (status, output) = self.target.run("connmanctl services | grep %s" % ssid)
+            retry = retry + 1
+            if (status == 0):
+                break
+        self.assertEqual(status, 0, msg="Not found AP service")
+        service = output.split(" ")[-1] 
+
+        # Do connection
+        exp = os.path.join(os.path.dirname(__file__), "files/wifi_connect.exp")
+        cmd = "expect %s %s %s %s %s" % (exp, target_ip, "connmanctl", service, pwd) 
+        status, output = shell_cmd_timeout(cmd, timeout=200)
+        self.assertEqual(status, 2, msg="Error messages: %s" % output)
+        # Check ip address by ifconfig command
+        (status, wifi_interface) = self.target.run("ifconfig | grep '^wlp' | awk '{print $1}'")
+        (status, output) = self.target.run("ifconfig %s | grep 'inet addr:'" % wifi_interface)
+        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+
+    @tag(FeatureID="IOTOS-528")
+    def test_wifi_connect_internet(self):
+        '''connmanctl to connect to internet, by broadcast wifi AP'''
+        target_ip = self.target.ip 
+        ssid = ssid_config.get("Connect","ssid_broadcast")
+        pwd = ssid_config.get("Connect","passwd_broadcast")
+
+        # For broadcast AP, get its service firstly.
+        retry = 0
+        while (retry < 4):
+            (status, output) = self.target.run('connmanctl scan wifi')
+            self.assertEqual(status, 0, msg="Error messages: %s" % output)
+            (status, output) = self.target.run("connmanctl services | grep %s" % ssid)
+            retry = retry + 1
+            if (status == 0):
+                break
+        self.assertEqual(status, 0, msg="Not found AP service")
+        service = output.split(" ")[-1] 
+
+        # Do connection
+        exp = os.path.join(os.path.dirname(__file__), "files/wifi_connect.exp")
+        cmd = "expect %s %s %s %s %s" % (exp, target_ip, "connmanctl", service, pwd) 
+        status, output = shell_cmd_timeout(cmd, timeout=200)
+        self.assertEqual(status, 2, msg="Error messages: %s" % output)
+        # Check ip address by ifconfig command
+        (status, wifi_interface) = self.target.run("ifconfig | grep '^wlp' | awk '{print $1}'")
+        (status, output) = self.target.run("ifconfig %s | grep 'inet addr:'" % wifi_interface)
+        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+
+        # Ping internet web links
+        (status, output) = self.target.run("ping www.baidu.com -c 4")
+        self.assertEqual(status, 0, msg="Error messages: %s" % output) 
