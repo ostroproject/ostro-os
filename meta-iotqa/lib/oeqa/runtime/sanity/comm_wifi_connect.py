a44b2f9aae3cc8f3fe0680790781f051a30e42b8
Zhang Jingke <jingke.zhang@intel.com>
2015-09-15 20:23:30 +0800

    meta-iotqa: support both broadcast and hidden AP connection.
    In sanity test, config.ini is able to support broadcast AP type.
    
    (From meta-iotqa rev: d407d34cf74df49f1625b630867caab731051bca)
    
    Signed-off-by: Zhang Jingke <jingke.zhang@intel.com>

--- meta-iotqa/lib/oeqa/runtime/sanity/comm_wifi_connect.py
+++ meta-iotqa/lib/oeqa/runtime/sanity/comm_wifi_connect.py
@@ -13,27 +13,44 @@ ssid_config.readfp(open(config_path))
 
 @tag(TestType="Functional Positive")
 class CommWiFiConect(oeRuntimeTest):
-    hidden_service = ""
+    service = ""
     def setUp(self):
         # un-block software rfkill lock
         self.target.run('rfkill unblock all')
         # Enable WiFi
+        self.target.run('connmanctl disable wifi')
+        time.sleep(1)
         (status, output) = self.target.run('connmanctl enable wifi')
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
         time.sleep(30)
-        # Scan nearby to get service of none-encryption broadcasting ssid
-        hidden_str = "hidden_managed_psk"
-        # will do scan retry 3 times if needed
-        retry = 0
-        while (retry < 4):
-            (status, output) = self.target.run('connmanctl scan wifi')
-            self.assertEqual(status, 0, msg="Error messages: %s" % output)
-            (status, services) = self.target.run("connmanctl services | grep %s" % hidden_str)
-            retry = retry + 1
-            if (status == 0):
-                break
-        self.assertEqual(status, 0, msg="Not found hidden AP service")
-        self.hidden_service = services.strip()
+        m_type = ssid_config.get("Connect","type")
+        if (m_type == "broadcast"):
+            ssid = ssid_config.get("Connect","ssid")
+            # For broadcast AP, get its service firstly.
+            retry = 0
+            while (retry < 4):
+                (status, output) = self.target.run('connmanctl scan wifi')
+                self.assertEqual(status, 0, msg="Error messages: %s" % output)
+                (status, output) = self.target.run("connmanctl services | grep %s" % ssid)
+                retry = retry + 1
+                if (status == 0):
+                    break
+            self.assertEqual(status, 0, msg="Not found AP service")
+            self.service = output.split(" ")[-1]
+        else:
+            # Scan nearby to get service of none-encryption broadcasting ssid
+            hidden_str = "hidden_managed_psk"
+            # will do scan retry 3 times if needed
+            retry = 0
+            while (retry < 4):
+                (status, output) = self.target.run('connmanctl scan wifi')
+                self.assertEqual(status, 0, msg="Error messages: %s" % output)
+                (status, services) = self.target.run("connmanctl services | grep %s" % hidden_str)
+                retry = retry + 1
+                if (status == 0):
+                    break
+            self.assertEqual(status, 0, msg="Not found hidden AP service")
+            self.service = services.strip()
 
     def tearDown(self):
         ''' disable wifi after testing '''
@@ -47,8 +64,13 @@ class CommWiFiConect(oeRuntimeTest):
         pwd = ssid_config.get("Connect","passwd")
 
         # Do connection
-        exp = os.path.join(os.path.dirname(__file__), "files/wifi_hidden_connect.exp")
-        cmd = "expect %s %s %s %s %s %s" % (exp, target_ip, "connmanctl", self.hidden_service, ssid, pwd) 
+        m_type = ssid_config.get("Connect","type")
+        if (m_type == "broadcast"):
+            exp = os.path.join(os.path.dirname(__file__), "files/wifi_connect.exp")
+            cmd = "expect %s %s %s %s %s" % (exp, target_ip, "connmanctl", self.service, pwd)
+        else:
+            exp = os.path.join(os.path.dirname(__file__), "files/wifi_hidden_connect.exp")
+            cmd = "expect %s %s %s %s %s %s" % (exp, target_ip, "connmanctl", self.service, ssid, pwd)
         status, output = shell_cmd_timeout(cmd, timeout=60)
         self.assertEqual(status, 2, msg="Error messages: %s" % output)
         # Check ip address by ifconfig command
