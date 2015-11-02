c9a361b5360fa774799e0713e7b508d72327119a
Alexandru Cornea <alexandru.cornea@intel.com>
2015-11-02 15:17:27 +0200

    meta-intel-iot-security: notroot.py: Removed from meta-security-framework and added dependency on meta-security-smack
    Modified tests to properly display the dependency on meta-security-smack notroot.py
    Added comments to notroot.py with purpose and usage example.
    
    From meta-intel-iot-security rev: 05aeac0d26443b5730b8000f1ba605b0872fa62c

--- meta-intel-iot-security/meta-security-smack/lib/oeqa/runtime/files/notroot.py
+++ meta-intel-iot-security/meta-security-smack/lib/oeqa/runtime/files/notroot.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
-
+#
+# Script used for running executables with custom labels, as well as custom uid/gid
+# Process label is changed by writing to /proc/self/attr/curent
+#
+# Script expects user id and group id to exist, and be the same.
+#
+# From adduser manual: 
+# """By  default,  each  user  in Debian GNU/Linux is given a corresponding group 
+# with the same name. """
+#
+# Usage: root@desk:~# python notroot.py <uid> <label> <full_path_to_executable> [arguments ..]
+# eg: python notroot.py 1000 User::Label /bin/ping -c 3 192.168.1.1
+#
+# Author: Alexandru Cornea <alexandru.cornea@intel.com>
 import os
 import sys
 
