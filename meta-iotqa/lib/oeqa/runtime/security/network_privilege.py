8f1dafb806f6bb2d58f86f777126224b1ab3daf3
Alexandru Cornea <alexandru.cornea@intel.com>
2015-11-27 15:19:28 +0200

    meta-iotqa: network_privilege: add test for InternetAccess and remove old tests
    Updated network_privilege.py with tests for InternetAccessPrivilege.
    Internet connection is checked in multiple ways, for backup in case
    one fails: pinging intel.com and 8.8.8.8, wget-ing www.intel.com and
    www.google.com. Remove all application_privilege tests from manifest
    file, as they are covered in network_privilege.
    
    From meta-iotqa rev: ce688d3dcce249b9429e27d8a02bbf0acba892ef

--- meta-iotqa/lib/oeqa/runtime/security/network_privilege.py
+++ meta-iotqa/lib/oeqa/runtime/security/network_privilege.py
@@ -10,8 +10,8 @@ from oeqa.utils.helper import get_files_dir
 
 @tag(TestType = 'Functional Positive', FeatureID = 'IOTOS-807')
 class NetworkPrivilege(oeRuntimeTest):
-    """ 
-    Testing network privilege enforcement
+    """
+    Testing local network and internet privilege enforcement
     Dependent on app-runas and smack
     """
 
@@ -21,7 +21,7 @@ class NetworkPrivilege(oeRuntimeTest):
         status, output = self.target.run(idcmd)
         if status:
                 status, output = self.target.run("adduser -D %s" %self.user)
-                self.assertFalse(status, msg="Adding app-privilege-user failed: %s" %output)
+                self.assertFalse(status, msg="Adding %s user failed: %s" %(self.user, output))
                 status, output = self.target.run(idcmd)
         self.assertTrue(output.isdigit(), msg="Unexpected output from %s: %s" %(idcmd, output))
         self.uid = output
@@ -34,7 +34,7 @@ class NetworkPrivilege(oeRuntimeTest):
                         "/tmp/app-runas")
 
         status, output = self.target.run("mount | grep smackfs ")
-        if status != 0: 
+        if status != 0:
             skipModule("Smack is not mounted")
         self.smack_path = output.split(" ")[2]
 
@@ -42,6 +42,11 @@ class NetworkPrivilege(oeRuntimeTest):
         status, output = self.target.run("ping -c 1 %s" %self.target.ip)
         return not(bool(status))
 
+    def _wait_smacknet(self):
+        status = 1
+        while status != 0:
+            status, output = self.target.run("cat %s/netlabel | grep Network::Local" %self.smack_path)
+            sleep(10)
 
     def test_app_no_network_privilege(self):
         """ Check if application without network privilege can access the network"""
@@ -60,8 +65,10 @@ class NetworkPrivilege(oeRuntimeTest):
         status, output = self.target.run("cat %s/load2 | grep 'Network::Local User::App::%s w'" \
                                             %(self.smack_path, appid))
         self.assertNotEqual(status, 0, "Smack rule for network to have write access to app " + \
-                                        "without privilegeshould not be set")
-        # check ping    
+                                        "without privilege should not be set")
+
+        self._wait_smacknet()
+        # check ping
         status, output = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'ping -c 3 %s'" \
                                             %(appid, self.uid, self.target.server_ip))
 
@@ -86,12 +93,13 @@ class NetworkPrivilege(oeRuntimeTest):
                                             %(self.smack_path, appid))
         self.assertEqual(status, 0, "Smack rule for network to have write access to app with " + \
                                     "local network privilege should be set. %s" %output)
-                
+
+        self._wait_smacknet()
         # check ping
         status, output = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'ping -c 3 %s'" \
                                         %(appid, self.uid, self.target.server_ip))
         self.assertIn("3 packets transmitted, 3 packets received, 0% packet loss", \
-                        output, "Application with network privilege cannot access network")
+                        output, "Application with network privilege cannot access network %s" %self.target.server_ip)
 
     def test_network_privilege_persistent_rules(self):
         """ Check app smack rules are persistent, remain after reboot """
@@ -101,7 +109,7 @@ class NetworkPrivilege(oeRuntimeTest):
         # install app
         self.target.run("/tmp/app-runas -a %s -p %s -u %s -r LocalNetworkAccess -i" \
                             %(appid, self.pkgid, self.uid))
-        
+
         # check smack rule
         status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Local w'" \
                                             %(self.smack_path, appid))
@@ -112,12 +120,12 @@ class NetworkPrivilege(oeRuntimeTest):
                                             %(self.smack_path, appid))
         self.assertEqual(status, 0, "Smack rule for network to have write access to app with " + \
                                     "local network privilege should be set")
-            
+
         # reboot
         self.target.run("reboot &")
         # give it time to shutdown
         sleep(10)
-        no_tries = 10 
+        no_tries = 10
 
         # wait for target to be alive
         while no_tries > 0 and not(self._alive()):
@@ -137,7 +145,7 @@ class NetworkPrivilege(oeRuntimeTest):
                                             %(self.smack_path, appid))
         self.assertEqual(status, 0, "Smack rule for network to have write access to app " + \
                                     "was removed after reboot")
-                
+
     def test_network_privilege_remove_app(self):
         appid = "test-remove-app"
 
@@ -170,3 +178,178 @@ class NetworkPrivilege(oeRuntimeTest):
                                             %(self.smack_path, appid))
         self.assertNotEqual(status, 0, "Smack rule for network to have write access to app with " + \
                                     "local network privilege kept after app removal")
+
+    def test_app_no_internet_privilege(self):
+        """ Check if application without internet privilege can access the cloud"""
+
+        appid = "test-app-no-internet-privilege"
+        # install app
+        self.target.run("/tmp/app-runas -a %s -p %s -u %s -i" % \
+                        (appid, self.pkgid, self.uid))
+
+        # check smack rule
+        status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Cloud w'" \
+                                            %(self.smack_path, appid))
+        self.assertNotEqual(status, 0, "Smack rule for app without internet privilege to have " + \
+                                        "write access to cloud should not be set")
+
+        status, output = self.target.run("cat %s/load2 | grep 'Network::Cloud User::App::%s w'" \
+                                            %(self.smack_path, appid))
+        self.assertNotEqual(status, 0, "Smack rule for cloud to have write access to app " + \
+                                        "without privilege should not be set")
+
+        self._wait_smacknet()
+        # check Internet Access
+        # ping might not always work, proxies may be in the way
+        # it is better to have some backup
+        status, output1 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'ping -c 3 www.intel.com'" \
+                                            %(appid, self.uid))
+        status1 = int(" 0% packet loss" not in output)
+
+        status, output2 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'ping -c 3 8.8.8.8'" \
+                                            %(appid, self.uid))
+        status2 = int(" 0% packet loss" not in output)
+
+        status, output3 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'wget -T 3 -P /tmp/ www.intel.com'" \
+                                            %(appid, self.uid))
+        # if wget succeeded, index.html file will be created.
+        # removing it will return 0 if the file exists
+        status3, output = self.target.run("rm /tmp/index.html")
+
+        status, output4 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'wget -T 3 -P /tmp/ www.google.com'" \
+                                            %(appid, self.uid))
+        # if wget succeeded, index.html file will be created.
+        # removing it will return 0 if the file exists
+        status4, output = self.target.run("rm /tmp/index.html")
+
+        # if any of the above succeded, then status is True
+        status = status1 and status2 and status3 and status4
+        output = output1 + output2 + output3 + output4
+        self.assertNotEqual(status, 0, "Application without internet privilege can access it. Output: %s" %output)
+
+    def test_app_with_internet_privilege(self):
+        """ Check if application with internet privilege can access the cloud"""
+
+
+        appid = "test-app-with-internet-privilege"
+        # install app
+        self.target.run("/tmp/app-runas -a %s -p %s -u %s -r InternetAccess -i" \
+                                    %(appid, self.pkgid, self.uid))
+
+        # check smack rule
+        status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Cloud w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for app with internet privilege to have " + \
+                                    "write access to cloud should be set %s" %output )
+
+        status, output = self.target.run("cat %s/load2 | grep 'Network::Cloud User::App::%s w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for cloud to have write access to app with " + \
+                                    "internet privilege should be set. %s" %output)
+
+        self._wait_smacknet()
+        # check Internet Access
+        # ping might not always work, proxies may be in the way
+        # it is better to have some backup
+        status, output1 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'ping -c 3 www.intel.com'" \
+                                            %(appid, self.uid))
+        status1 = int(" 0% packet loss" not in output1)
+
+        status, output2 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'ping -c 3 8.8.8.8'" \
+                                            %(appid, self.uid))
+        status2 = int(" 0% packet loss" not in output2)
+
+        status, output3 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'wget -T 3 -P /tmp/ www.intel.com'" \
+                                            %(appid, self.uid))
+        # if wget succeeded, index.html file will be created.
+        # removing it will return 0 if the file exists
+        status3, output = self.target.run("rm /tmp/index.html")
+
+        status, output4 = self.target.run("/tmp/app-runas -a %s -u %s -e -- sh -c 'wget -T 3 -P /tmp/ www.google.com'" \
+                                            %(appid, self.uid))
+        # if wget succeeded, index.html file will be created.
+        # removing it will return 0 if the file exists
+        status4, output = self.target.run("rm /tmp/index.html")
+
+        # if any of the above attempts passed, then status should be True
+        status = status1 and status2 and status3 and status4
+        output = output1 + output2 + output3 + output4
+        self.assertEqual(status, 0, "Application with internet privilege cannot access it. Output: %s" %output)
+
+    def test_internet_privilege_persistent_rules(self):
+        """ Check app smack rules are persistent, remain after reboot """
+
+        appid = "test-persistent-rules"
+
+        # install app
+        self.target.run("/tmp/app-runas -a %s -p %s -u %s -r InternetAccess -i" \
+                            %(appid, self.pkgid, self.uid))
+
+        # check smack rule
+        status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Cloud w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for app with internet privilege to have " + \
+                                    "write access to cloud should be set")
+
+        status, output = self.target.run("cat %s/load2 | grep 'Network::Cloud User::App::%s w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for cloud to have write access to app with " + \
+                                    "internet privilege should be set")
+
+        # reboot
+        self.target.run("reboot &")
+        # give it time to shutdown
+        sleep(10)
+        no_tries = 10
+
+        # wait for target to be alive
+        while no_tries > 0 and not(self._alive()):
+            sleep(20)
+            no_tries -= 1
+
+        if not self._alive():
+           self.fail("Target did not come up after reboot")
+
+        # check smack rule after reboot
+        status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Cloud w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for app to have write access to cloud " + \
+                                    "was removed after reboot " )
+
+        status, output = self.target.run("cat %s/load2 | grep 'Network::Cloud User::App::%s w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for cloud to have write access to app " + \
+                                    "was removed after reboot")
+
+    def test_internet_privilege_remove_app(self):
+        appid = "test-internet-remove-app"
+
+        # install app
+        self.target.run("/tmp/app-runas -a %s -p %s -u %s -r InternetAccess -i" % \
+                            (appid, self.pkgid, self.uid))
+
+        # check smack rule
+        status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Cloud w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for app with internet privilege to have " + \
+                                    "write access to cloud should be set")
+
+        status, output = self.target.run("cat %s/load2 | grep 'Network::Cloud User::App::%s w'" \
+                                            %(self.smack_path, appid))
+        self.assertEqual(status, 0, "Smack rule for cloud to have write access to app with " + \
+                                    "internet privilege should be set")
+
+        # remove app
+        self.target.run("/tmp/app-runas -a %s -p %s -u %s -d" % \
+                            (appid, self.pkgid, self.uid))
+
+        # check smack rule
+        status, output = self.target.run("cat %s/load2 | grep 'User::App::%s Network::Cloud w'" \
+                                            %(self.smack_path, appid))
+        self.assertNotEqual(status, 0, "Smack rule for app with internet privilege to have " + \
+                                    "write access to cloud kept after app removal")
+
+        status, output = self.target.run("cat %s/load2 | grep 'Network::Cloud User::App::%s w'" \
+                                            %(self.smack_path, appid))
+        self.assertNotEqual(status, 0, "Smack rule for cloud to have write access to app with " + \
+                                    "internet privilege kept after app removal")
