013eafb11e967a44acb8078f597a96b382e3d18e
Li Min <min.a.li@intel.com>
2015-12-25 14:45:53 +0800

    meta-iotqa: PnP: Update the parsing result method for disksize From the df command's output, the disk size info may be at the next line. The default result parsing method doesn't work for the exception, which also depends on awk and grep commands. This issue will be fixed by this commit.
    (From meta-iotqa rev: 18632d1d5ab158872b534f66fcded3d7f061440a)
    
    Signed-off-by: Li Min <min.a.li@intel.com>

--- meta-iotqa/lib/oeqa/runtime/pnp/disksize.py
+++ meta-iotqa/lib/oeqa/runtime/pnp/disksize.py
@@ -16,6 +16,7 @@
 
 import os
 import time
+import re
 from oeqa.oetest import oeRuntimeTest
 from oeqa.utils.helper import collect_pnp_log
 
@@ -24,7 +25,41 @@ class DiskSizeTest(oeRuntimeTest):
     @class DiskSizeTest
     """
 
-    
+
+    def _parse_result(self, casename, logname):
+        """parse disk size info from log file
+        @param self
+        @param casename
+        @param logname
+        @return disksize
+        """
+        logpath = os.path.join(".", casename, logname)
+        file = open(logpath, 'r')
+        lines = file.readlines()
+        max = len(lines)
+        disksize = ""
+        index = max -1
+        while index > 0:
+            #search the latest data from the end line
+            if '/dev/disk' in lines[index]:
+                pattern = re.compile(r'(\d*\.?\d*[GM])')
+                size = pattern.findall(lines[index])
+                if len(size) > 1:
+                    disksize = size[1]
+                    break
+                else:
+                    #The result may be at the next line
+                    size = pattern.findall(lines[index+1])
+                    if len(size) > 1:
+                        disksize = size[1]
+                        break
+                    else:
+                        print "Failed to find disk size!"
+                        break
+            index = index -1
+        file.close()
+        return disksize
+
     def test_disksize(self):
         """use df command to calculate the image installed size
         @fn test_disksize
@@ -33,19 +68,18 @@ class DiskSizeTest(oeRuntimeTest):
         """
         filename = os.path.basename(__file__)
         casename = os.path.splitext(filename)[0]
-        (status, output) = self.target.run(
-            "df | grep '/dev' | "
-            "awk '{sum+=$3} END {print sum/1024}'")
-        collect_pnp_log(casename, casename, output)
-        print "\n%s:%s\n" % (casename, output)
-        ##
-        # TESTPOINT: #1, test_disksize
-        #
-        self.assertEqual(status, 0, output)
-
         (status, output) = self.target.run("df -h")
         logname = casename + "-detail"
         collect_pnp_log(casename, logname, output)
+        disksize = self._parse_result(casename,logname)
+        collect_pnp_log(casename, casename, disksize)
+        ##
+        # TESTPOINT: #1, test_disksize
+        #
+        print "\n%s:%s\n" % (casename, output)
+        self.assertEqual(status, 0, output)
+
+
         
 
 
