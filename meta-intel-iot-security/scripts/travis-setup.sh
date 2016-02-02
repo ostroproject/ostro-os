0109404ec8336ca428d9423a3b3fe51a5f2dfa90
Patrick Ohly <patrick.ohly@intel.com>
2016-02-02 12:18:58 +0100

    meta-intel-iot-security: travis: add build statistics
    Inheriting buildstats-summary adds some sstate statistics
    to each bitbake invocation.
    
    (From meta-intel-iot-security rev: 49deb808c7087509d9bf5aa84a36291f6f998b61)
    
    Signed-off-by: Patrick Ohly <patrick.ohly@intel.com>

--- meta-intel-iot-security/scripts/travis-setup.sh
+++ meta-intel-iot-security/scripts/travis-setup.sh
@@ -93,6 +93,9 @@ VIRTUAL-RUNTIME_initscripts = ""
 CORE_IMAGE_EXTRA_INSTALL_append_pn-core-image-minimal = " smack-userspace security-manager security-manager-policy cynara app-runas"
 INHERIT_append_pn-core-image-minimal = " ima-evm-rootfs"
 
+# Dump information about sstate usage.
+INHERIT += "buildstats-summary"
+
 # For testing...
 EXTRA_IMAGE_FEATURES = "debug-tweaks ssh-server-dropbear"
 CORE_IMAGE_EXTRA_INSTALL_append_pn-core-image-minimal = " python"
