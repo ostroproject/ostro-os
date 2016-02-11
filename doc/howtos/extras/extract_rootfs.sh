cde83b335a3f136839a2b3ba6301968815d77ad6
Dmitry Rozhkov <dmitry.rozhkov@intel.com>
2016-02-11 16:01:15 +0200

    meta-ostro: swupd howto: Make extract_rootfs.sh better conform bashism-free coding style
    When single brackes are used the argument may get split and it's safer
    to wrap it in quotas always.
    
    (From meta-ostro rev: 6afdac553e42cb4a8ba01138bb237533dd96f6e7)
    
    Signed-off-by: Dmitry Rozhkov <dmitry.rozhkov@intel.com>

--- doc/howtos/extras/extract_rootfs.sh
+++ doc/howtos/extras/extract_rootfs.sh
@@ -11,25 +11,25 @@ function usage {
     echo "Usage: $0 <path_to_image_file> <storage_directory> <version> <rootfs_partition>"
 }
 
-if [ -z $IMG_FILE ]; then
+if [ -z "${IMG_FILE}" ]; then
     echo "ERROR: No image file specified."
     usage
     exit 1
 fi
 
-if [ -z $STORAGE_DIR ]; then
+if [ -z "${STORAGE_DIR}" ]; then
     echo "ERROR: No storage directory specified"
     usage
     exit 1
 fi
 
-if [ -z $VER ]; then
+if [ -z "${VER}" ]; then
     echo "ERROR: No version specified"
     usage
     exit 1
 fi
 
-if [ -z $PARTITION_NUM ]; then
+if [ -z "${PARTITION_NUM}" ]; then
     echo "ERROR: No partition number specified. You can find the number corresponding"
     echo "       to the rootfs partition in the file"
     echo "       meta-ostro/recipes-image/images/files/iot-cfg/disk-layout-<MACHINE>.json"
