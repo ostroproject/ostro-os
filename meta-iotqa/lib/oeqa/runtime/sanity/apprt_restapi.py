cf3f7fa0d9a1525af196fd00cb9cf663fff1b9f5
wanghongjuan <hongjuanx.wang@intel.com>
2016-06-20 18:21:50 +0800

    meta-iotqa: Add some conditions to check the restapi server
    Related to: IOTOS-1650
    
    (From meta-iotqa rev: 9a8f7e494907c6c3c5ee4b76f1367365902794c2)
    
    Signed-off-by: Wang Hongjuan <hongjuan.wang@intel.com>

--- meta-iotqa/lib/oeqa/runtime/sanity/apprt_restapi.py
+++ meta-iotqa/lib/oeqa/runtime/sanity/apprt_restapi.py
@@ -40,8 +40,17 @@ class SanityTestRestApi(oeRuntimeTest):
         '''
         Send a HTTP request to the REST API server and see if 200 is returned.
         '''
+        enable_port_cmd = 'iptables -A INPUT -p tcp -dport 8000 -j ACCEPT'
+        start_server_cmd = 'systemctl start iot-rest-api-server'
+        self.target.run(enable_port_cmd)
+        self.target.run(start_server_cmd)
+        check_process_cmd = 'ps | grep "iot-rest-api-server" | grep -v grep | awk "{print $1}"'
+        (status, output) = self.target.run(check_process_cmd)
+        self.assertNotEqual(output, ' ', 'Can not start the iot-rest-api-server')
         (returncode, output) = self._run_curl_cmd()
         self.assertEqual(output.strip(), '200')
+        stop_server_cmd = 'systemctl stop iot-rest-api-server'
+        self.target.run(stop_server_cmd)
 
 ##
 # @}
